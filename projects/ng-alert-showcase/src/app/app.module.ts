8b89228cf55c4a67ef9eaf1c0dd8a6b2dbc395b3
antoniofc <antoniofc@gmail.com>
2019-12-03 11:39:33 -0300

    Segundo commit

---
+++ projects/ng-alert-showcase/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgSimpleAlertModule } from './../../../ng-simple-alert/src/lib/ng-simple-alert.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgSimpleAlertModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ projects/ng-simple-alert/src/lib/ng-simple-alert.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgSimpleAlertComponent } from './ng-simple-alert.component';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [NgSimpleAlertComponent],
+  imports: [ CommonModule ],
+  exports: [NgSimpleAlertComponent]
+})
+export class NgSimpleAlertModule { }
